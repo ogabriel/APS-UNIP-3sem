d7afdc257cd6251ef00a54c9d895ffcb6e6ab896
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2018-05-26 13:23:03 -0300

    Insercao do metodo de mensagens

--- src/application/Launcher.java
+++ src/application/Launcher.java
@@ -4,7 +4,14 @@ public class Launcher {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		mensagem();
+		MainMenu menu = new MainMenu();
+		mensagem();		
+		
+	}
+	public static void mensagem() {
+		System.out.println("\n _   _  _   _  _  ____\n| | | || \\ | ||	||    |\n| |_| ||  \\| || ||  __|\n|_____||_|\\__||_||_|");
+        System.out.println("CIÊNCIA DA COMPUTAÇÃO\nAPS 2018 - 3ºSEMESTRE\n ");
 	}
 
 }
