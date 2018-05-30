76cda52061db4b7419c43b4e5d776a091920c595
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2018-05-29 22:36:10 -0300

    Herdando Mensagens e adicionando metodo

--- src/application/Problemas.java
+++ src/application/Problemas.java
@@ -1,5 +1,24 @@
 package application;
 
-public class Problemas {
-
+public class Problemas extends Mensagens{
+	public String escolha(int num) {
+		String mensagem = "";
+		switch (num) {
+		case 1:
+			mensagem = "Solução";
+			break;
+		case 2:
+			mensagem = "Próxima";
+			break;
+		case 3:
+			mensagem = "Sair";
+			break;
+		case 4:
+			mensagem = "Sair";
+			break;	
+		
+		}
+		
+		return mensagem;
+	}
 }
