ede5de4e5ce6903b8d7689e1385945f96cdcaab2
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2018-05-29 22:30:28 -0300

    Herdando Mensagens e adicionado metodos

---
+++ src/application/Denunciar.java
@@ -0,0 +1,20 @@
+package application;
+
+public class Denunciar extends Mensagens {
+	public String escolha(int num) {
+		String mensagem = "";
+		
+		switch(num) {
+			case 1:
+				mensagem = "Ibama";
+			break;
+			case 2:
+				mensagem = "Polícia ambiental";
+			break;
+			case 3:
+				mensagem = "Sair";
+			break;
+		}
+		return mensagem;
+	}
+}
