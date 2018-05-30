9e9dee2933fde0afdc38e727f2744a4ddc2c9747
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2018-05-30 11:13:22 -0300

    Adicionando overload de menu() e input()

---
+++ src/application/Menu.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.Scanner;
+
+public class Menu {
+	
+	public static void menu(String one, String two, String three, String four) {
+		menu(one, two, three);
+		System.out.println("4 - " + four);
+	}
+	
+	public static void menu(String one, String two, String three) {
+		menu(one, two);
+		System.out.println("3 - " + three);
+		
+	}
+	public static void menu(String one, String two) {
+		System.out.println("1 - " + one);
+		System.out.println("2 - " + two);
+	}
+	
+	public static int input() {
+		Scanner scan = new Scanner(System.in);
+		scan.close();
+		String s = scan.next();
+		int num = Integer.parseInt(s);
+		
+		return num;
+	}
+}
