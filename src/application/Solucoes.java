4f1fa3c81c09edbd0b0bbf1755991faae3d4edb5
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2018-05-31 18:49:23 -0300

    Corrigindo Solucoes e escrevendo continueMenu()

--- src/application/Solucoes.java
+++ src/application/Solucoes.java
@@ -1,16 +1,44 @@
 package application;
 
 public class Solucoes extends Mensagens implements Escolhas{
-	public String escolha(int resposta) {
-		String mensagem = "";
+
+	@Override
+	public void continueMenu() {
+		boolean exit;
+		do {
+			Menu.menu("Denunciar problema", "Sair");
+			int opcao = Menu.input();
+			exit = escolha(opcao);
+		} while (exit);
+	}
+	
+	@Override
+	public boolean escolha(int resposta) {
 		switch (resposta) {
 		case 1:
-			mensagem = "Denunciar";
-			break;
+			Denuncias d = new Denuncias();
+			d.continueMenu();
+			d = null;
+			return false;
 		case 2:
-			mensagem = "Sair";
-			break;
+			return true;		
+		default:
+			Menu.show("Opção invalida");
+			return false;
 		}
-		return mensagem;
+	}
+
+	@Override
+	public String mensagem() {
+		switch (this.opcao) {
+		case 1:
+			return "mensagem1";
+		case 2:
+			return "mensagem2";
+		case 3:
+			return "mensagem3";
+		default:
+			return null;
+		}
 	}
 }
