d2496dea5d7fde722160c6521ff359be44fe2f20
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2018-05-30 23:17:11 -0300

    Adicionando suite de testes

---
+++ src/tests/AllTests.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({TestDenuncias.class, TestProblemas.class, TestSolucoes.class})
+public class AllTests {
+	
+}
