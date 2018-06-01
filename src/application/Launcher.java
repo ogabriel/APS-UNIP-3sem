68b448064b5ee332b658508eab8ae3261fb67cb2
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2018-06-01 02:26:53 -0300

    Adicionando entrada pra Engine()

--- src/application/Launcher.java
+++ src/application/Launcher.java
@@ -4,11 +4,13 @@ public class Launcher {
 
 	public static void main(String[] args) {
 		//colocar metodo iniciacao do engine
-		mensagem();
-		mensagem();		
+		logo();
+		Engine e = new Engine();
+		e.continueMenu();
+		logo();		
 		
 	}
-	public static void mensagem() {
+	public static void logo() {
 		System.out.println("\n _   _  _   _  _  ____\n| | | || \\ | ||	||    |\n| |_| ||  \\| || ||  __|\n|_____||_|\\__||_||_|");
         System.out.println("CIÊNCIA DA COMPUTAÇÃO\nAPS 2018 - 3ºSEMESTRE\n ");
 	}
